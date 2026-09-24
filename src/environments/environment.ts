d63361aad36374fb37e59ee144f2d92e54374d10
Rodolxde <gutierrezrodolfo360@gmail.com>
2026-09-24 12:59:28 -0500

    feat(environments): add environments files for development and production configuration

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+/**
+ * @summary Development environment configuration for the application.
+ * This file contains settings and variables specific to the development environment.
+ * @author Rodolfo Zavaleta U20241F733
+ */
+export const environment = {
+  production: true,
+  hipolabsApiBaseUrl: 'http://universities.hipolabs.com',
+  universitiesHipolabsUniversitiesEndpointPath: '/search',
+  logoHipolabsApiBaseUrl: 'http://img.logo.dev',
+  logoHipolabsPublishabledKey: 'YOUR_LOGO_PROVIDER_PUBLISHABLED_KEY',
+};
